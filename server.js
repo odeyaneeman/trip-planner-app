bca73f6e0bc6b9c416ebb6aaec29f38d9d6d2100
odeyaneeman <odeya.neeman@gmail.com>
2026-06-22 16:31:19 +0300

    fix: double underscore for dirname

--- server.js
+++ server.js
@@ -187,7 +187,7 @@ app.post('/api/login', async (req, res) => {
 
 const path = require('path');
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+res.sendFile(path.join(__dirname, 'home.html'));
 });
 
 
